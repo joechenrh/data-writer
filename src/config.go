29845115d3830ab7f64b723b842e37b6e3afc047
Ruihao Chen <ruihao.chen@pingcap.cn>
2025-06-12 14:32:22 +0800

    Update

---
+++ src/config.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+
+	"github.com/pingcap/tidb/br/pkg/storage"
+)
+
+type S3Config struct {
+	Region          string `toml:"region,omitempty"`
+	AccessKey       string `toml:"access_key,omitempty"`
+	SecretAccessKey string `toml:"secret_key,omitempty"`
+	Provider        string `toml:"provider,omitempty"`
+	Endpoint        string `toml:"endpoint,omitempty"`
+	Force           bool   `toml:"force,omitempty"`
+}
+
+type GCSConfig struct {
+	Credential string `toml:"credential,omitempty"`
+}
+
+type CommonConfig struct {
+	Path        string `toml:"path"`
+	Prefix      string `toml:"prefix"`
+	Folders     int    `toml:"folders"`
+	StartFileNo int    `toml:"start_fileno"`
+	EndFileNo   int    `toml:"end_fileno"`
+	Rows        int    `toml:"rows"`
+	FileFormat  string `toml:"format"`
+}
+
+type ParquetConfig struct {
+	PageSizeKB   int64 `toml:"page_size_kb"`
+	NumRowGroups int   `toml:"row_groups"`
+}
+
+type CSVConfig struct {
+	Base64 bool `toml:"base64"`
+}
+
+type Config struct {
+	Common    CommonConfig  `toml:"common"`
+	Parquet   ParquetConfig `toml:"parquet"`
+	CSV       CSVConfig     `toml:"csv"`
+	S3Config  *S3Config     `toml:"s3,omitempty"`
+	GCSConfig *GCSConfig    `toml:"gcs,omitempty"`
+}
+
+// GetStore initializes and returns an ExternalStorage instance based on the provided configuration.
+func GetStore(c Config) (storage.ExternalStorage, error) {
+	var op *storage.BackendOptions
+	if c.S3Config != nil {
+		op = &storage.BackendOptions{S3: storage.S3BackendOptions{
+			Region:          c.S3Config.Region,
+			AccessKey:       c.S3Config.AccessKey,
+			SecretAccessKey: c.S3Config.SecretAccessKey,
+			Provider:        c.S3Config.Provider,
+			Endpoint:        c.S3Config.Endpoint,
+		}}
+	} else if c.GCSConfig != nil {
+		op = &storage.BackendOptions{GCS: storage.GCSBackendOptions{
+			CredentialsFile: c.GCSConfig.Credential,
+		}}
+	}
+
+	s, err := storage.ParseBackend(c.Common.Path, op)
+	if err != nil {
+		return nil, err
+	}
+
+	return storage.NewWithDefaultOpt(context.Background(), s)
+}
--- src/spec.go
+++ src/spec.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
+	"math"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/apache/arrow-go/v18/parquet"
+	"github.com/google/uuid"
+	"github.com/pingcap/tidb/pkg/util/hack"
 )
 
 type FieldType struct {
@@ -149,4 +157,203 @@ func getSpecFromSQL(sqlPath string) []ColumnSpec {
 	}
 
 	return specs
+}
+
+func (c *ColumnSpec) generatePartialOrderInt(rowID int) int {
+	randPrefix := (rowID * 1000000007) & 0x0000000000000007
+	moveBit := c.length - 5
+	return (randPrefix << moveBit) + rowID
+}
+
+func (c *ColumnSpec) generateGaussianInt(rng *rand.Rand) int {
+	randomFloat := (rng.Float64()-0.5)*2*float64(c.StdDev) + float64(c.Mean)
+	randomInt := int(math.Round(randomFloat))
+
+	if c.length == 64 {
+		return randomInt
+	}
+
+	lower := 0
+	upper := 1<<c.length - 1
+	if c.Signed {
+		lower -= (1 << (c.length - 1))
+		upper -= (1 << (c.length - 1))
+	}
+
+	if randomInt > upper {
+		randomInt = upper
+	} else if randomInt < lower {
+		randomInt = lower
+	}
+	return randomInt
+}
+
+func (c *ColumnSpec) generateRandomInt() int {
+	if c.length == 64 {
+		return rand.Int()
+	}
+
+	v := rand.Intn(1 << c.length)
+	if c.Signed {
+		v -= 1 << (c.length - 1)
+	}
+	return v
+}
+
+func (c *ColumnSpec) generateInt(rowID int, rng *rand.Rand) int {
+	// gaussian distribution
+	if c.StdDev > 0 {
+		return c.generateGaussianInt(rng)
+	}
+
+	if c.IsUnique || c.IsPrimary {
+		return rowID
+	}
+
+	switch c.Order {
+	case NumericNoOrder:
+		return c.generateRandomInt()
+	case NumericTotalOrder:
+		return rowID
+	case NumericPartialOrder:
+		if rowID%128 == 0 {
+			return c.generatePartialOrderInt(rowID)
+		}
+		return rowID
+	case NumericRandomOrder:
+		return c.generatePartialOrderInt(rowID)
+	default:
+		log.Printf("Unsupported order: %d", c.Order)
+	}
+	return rowID
+}
+
+func (c *ColumnSpec) generateNULL(rng *rand.Rand) bool {
+	return rng.Intn(100) < c.NullPercent
+}
+
+func (c *ColumnSpec) generateBatchNull(length int, rng *rand.Rand) []bool {
+	randomIndices := make([]byte, length)
+	rng.Read(randomIndices)
+
+	null := make([]bool, length)
+	for i := range length {
+		null[i] = int(randomIndices[i])*100 < c.NullPercent*256
+	}
+	return null
+}
+
+func (c *ColumnSpec) generateString(rng *rand.Rand) string {
+	if c.IsUnique || c.IsPrimary {
+		return uuid.New().String()
+	}
+
+	length := rng.Intn(c.length) + 1
+	b := make([]byte, length)
+	rng.Read(b)
+	for i := range b {
+		b[i] = b[i]%70 + 50
+	}
+
+	return string(hack.String(b))
+}
+
+func (c *ColumnSpec) generateRandomTimestamp() string {
+	// Get the current time
+	now := time.Now()
+
+	// Calculate the time one year ago
+	oneYearAgo := now.AddDate(-1, 0, 0)
+
+	// Generate a random duration between 0 and one year
+	randomDuration := time.Duration(rand.Int63n(int64(now.Sub(oneYearAgo))))
+
+	// Add the random duration to one year ago to get a random time within the last year
+	randomTime := oneYearAgo.Add(randomDuration)
+
+	// Format the random time into a string (e.g., RFC3339 format)
+	timestamp := randomTime.Format(time.RFC3339)
+
+	return timestamp
+}
+
+func (c *ColumnSpec) generate(rowID int, rng *rand.Rand) (any, int16) {
+	if c.generateNULL(rng) {
+		return "\\N", 0
+	}
+
+	switch c.SQLType {
+	case "int":
+		return c.generateInt(rowID, rng), 1
+	case "bigint":
+		return c.generateInt(rowID, rng), 1
+	case "float64":
+		return c.generateInt(rowID, rng), 1
+	case "char", "varchar", "varbinary":
+		return c.generateString(rng), 1
+	case "timestamp":
+		return c.generateRandomTimestamp(), 1
+	}
+	return nil, 0
+}
+
+func (c *ColumnSpec) generateBatchInt64(rowID int, out []int64, defLevel []int16, rng *rand.Rand) {
+	nullMap := c.generateBatchNull(len(out), rng)
+	for i := range len(out) {
+		if nullMap[i] {
+			defLevel[i] = 0
+		} else {
+			defLevel[i] = 1
+			out[i] = int64(c.generateInt(rowID+i, rng))
+		}
+	}
+}
+
+func (c *ColumnSpec) generateBatchFloat(rowID int, out []float64, defLevel []int16, rng *rand.Rand) {
+	nullMap := c.generateBatchNull(len(out), rng)
+	for i := range len(out) {
+		if nullMap[i] {
+			defLevel[i] = 0
+		} else {
+			defLevel[i] = 1
+			out[i] = float64(c.generateInt(rowID, rng))
+		}
+	}
+}
+
+func (c *ColumnSpec) generateBatchTimestamp(out []parquet.ByteArray, defLevel []int16, rng *rand.Rand) {
+	nullMap := c.generateBatchNull(len(out), rng)
+	for i := range len(out) {
+		if nullMap[i] {
+			defLevel[i] = 0
+		} else {
+			defLevel[i] = 1
+			out[i] = hack.Slice(c.generateRandomTimestamp())
+		}
+	}
+}
+
+func (c *ColumnSpec) generateBatchString(_ int, out []parquet.ByteArray, defLevel []int16, rng *rand.Rand) {
+	nullMap := c.generateBatchNull(len(out), rng)
+
+	slen := rng.Intn(c.length-1) + 1
+	buf := make([]byte, slen*len(out))
+	rng.Read(buf)
+	for i := range buf {
+		buf[i] = buf[i]%120 + 55
+	}
+
+	for i := range len(out) {
+		if nullMap[i] {
+			defLevel[i] = 0
+		} else {
+			defLevel[i] = 1
+			out[i] = buf[i*slen : (i+1)*slen]
+		}
+	}
+}
+
+func generateSingleField(rowID int, spec ColumnSpec, rng *rand.Rand) string {
+	v, _ := spec.generate(rowID, rng)
+	return fmt.Sprintf("%v", v)
 }
